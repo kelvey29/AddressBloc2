024fdaf0c5ecd4cefcea2b507564f666fc89b263
kelvey29 <kelvey29@gmail.com>
2017-05-10 02:18:03 +0000

    checkpoint-21-completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,37 +1,87 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+    let (:book) {AddressBook.new}
+    
     describe "attributes" do
        it "responds to entries" do
-          book = AddressBook.new
           expect(book).to respond_to(:entries)
        end
        
        it "initializes entries as an array" do
-          book = AddressBook.new
           expect(book.entries).to be_an(Array)
        end
        
        it "initializes entries as empty" do
-          book = AddressBook.new
           expect(book.entries.size).to eq(0)
        end
     end
     
     describe "#add_entry" do
        it "adds only one entry to the address book" do
-          book = AddressBook.new
           book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
           expect(book.entries.size).to eq(1)
        end
        
        it "adds the correct information to entries" do
-          book = AddressBook.new
           book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
           new_entry = book.entries[0]
           expect(new_entry.name).to eq('Ada Lovelace')
           expect(new_entry.phone_number).to eq('010.012.1815')
           expect(new_entry.email).to eq('augusta.king@lovelace.com')
+       end
+    end
+    
+    describe "#import_from_csv" do
+       it "imports the correct number of entries" do
+          book.import_from_csv("entries.csv")
+          book_size = book.entries.size
+          expect(book_size).to eq(5)
+       end
+       
+       it "imports the first entry" do
+          book.import_from_csv("entries.csv")
+          entry_one = book.entries[0]
+          
+          expect(entry_one.name).to eq "Bill"
+          expect(entry_one.phone_number).to eq "555-555-4854"
+          expect(entry_one.email).to eq "bill@blocmail.com"
+       end
+       
+       it "imports the 2nd entry" do
+          book.import_from_csv("entries.csv")
+          entry_two = book.entries[1]
+          
+          expect(entry_two.name).to eq "Bob"
+          expect(entry_two.phone_number).to eq "555-555-5415"
+          expect(entry_two.email).to eq "bob@blocmail.com"
+       end
+ 
+       it "imports the 3rd entry" do
+          book.import_from_csv("entries.csv")
+          entry_three = book.entries[2]
+          
+          expect(entry_three.name).to eq "Joe"
+          expect(entry_three.phone_number).to eq "555-555-3660"
+          expect(entry_three.email).to eq "joe@blocmail.com"
+       end
+ 
+       it "imports the 4th entry" do
+          book.import_from_csv("entries.csv")
+          entry_four = book.entries[3]
+          
+          expect(entry_four.name).to eq  "Sally"
+          expect(entry_four.phone_number).to eq "555-555-4646"
+          expect(entry_four.email).to eq "sally@blocmail.com"
+       end
+ 
+       it "imports the 5th entry" do
+          book.import_from_csv("entries.csv")
+          entry_five = book.entries[4]
+          
+          expect(entry_five.name).to eq "Sussie"
+          expect(entry_five.phone_number).to eq "555-555-2036"
+          expect(entry_five.email).to eq "sussie@blocmail.com"
        end
     end
 end
